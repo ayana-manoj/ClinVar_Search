5e5165a156dcfb8935a90af0710be94b579039ea
Harbi Nuur <harbi.nuur@postgrad.manchester.ac.uk>
2025-11-20 17:53:15 +0000

    https://github.com/ayana-manoj/ClinVar_Search/issues/17

--- ClinVar_Search/ClinVar_Site/app.py
+++ ClinVar_Search/ClinVar_Site/app.py
@@ -4,6 +4,9 @@ from flask import Flask, request, render_template, send_from_directory, jsonify,
 from werkzeug.utils import secure_filename
 from ClinVar_Search.modules.parser import parser
 from ClinVar_Search.logger import logger
+from ClinVar_Search.modules.dummy_database import sql_database, initialise_dummy_variables
+import sqlite3
+from sqlalchemy import text
 
 
 # Set up your app
@@ -13,17 +16,30 @@ Webapp = Flask(__name__)
 Upload_folder = "ClinVar_Search/ClinVar_Site/Clinvar_Uploads"
 Processed_folder = "ClinVar_Search/ClinVar_Site/Clinvar_Search_Outputs"
 Error_folder = "ClinVar_Search/ClinVar_Site/Clinvar_Error_Outputs"
+Database_folder = "ClinVar_Search/ClinVar_Site/Clinvar_Database_folder"
 allowed_ext = {"vcf", "csv"}
 
 # Configure folders in your app
 Webapp.config["Upload_folder"] = Upload_folder
 Webapp.config["Processed_folder"] = Processed_folder
 Webapp.config["Error_folder"] = Error_folder
+Webapp.config["Database_folder"] = Database_folder
+
 
 # Ensure directories exist
 os.makedirs(Upload_folder, exist_ok=True)
 os.makedirs(Processed_folder, exist_ok=True)
 os.makedirs(Error_folder, exist_ok=True)
+os.makedirs(Database_folder, exist_ok=True)
+
+#initialise database if it does not exist, ideally should not be made again although redundancies are in place in the module
+db_exists = os.path.isfile("ClinVar_Search/ClinVar_Site/Clinvar_Database_folder/ClinVar_Database.db")
+if db_exists:
+    patient_database = "ClinVar_Search/ClinVar_Site/Clinvar_Database_folder/ClinVar_Database.db"
+    Webapp.config["patient_database"] = patient_database 
+else:
+    patient_database = sql_database(), initialise_dummy_variables()
+
 
 # Allowed file extensions function
 def allowed_file(filename):
@@ -65,11 +81,15 @@ def save_output_to_file(content, title, folder=Processed_folder, overwrite=False
     return output_path, status
 def app_file_check(file_path, overwrite=False):
     """Check the file extension and process accordingly.
-    This is essentially the same as detemrine file type but cleaned upp and for an application"""
+    This is essentially the same as detemrine file type but cleaned up and for an application"""
+    #initialising required variables 
     file_end = Path(file_path).suffix.lower()
     title = Path(file_path).stem
+    misaligned_title = f"misaligned_{title}"
     saved_file = None
     misaligned_file = None
+
+    #logic for saving file depending on the conditions
     if file_end == ".csv" or file_end == ".vcf":
         processed_data, misaligned_data = parser(file_path)
     else:
@@ -78,7 +98,7 @@ def app_file_check(file_path, overwrite=False):
     saved_file, status = save_output_to_file(processed_data, title, folder=Processed_folder, overwrite=overwrite)  
         
     if misaligned_data:
-        misaligned_file, status = save_output_to_file(misaligned_data, title, folder=Error_folder, overwrite=overwrite)
+        misaligned_file, status = save_output_to_file(misaligned_data, misaligned_title, folder=Error_folder, overwrite=overwrite)
 
     return saved_file, misaligned_file, status
 #route for the index/ landing page
@@ -189,15 +209,19 @@ def download_file(filename):
 There are currently two sections, the database results and the processed data
 The processed data will show how the data has been processed, with the latest patient shown first
 This can give scientists the chance to see if the number of variants match up while also showing what our inputs for the API should look like
+There is also a chance to have a look at why the data did not process properly
 """
 @Webapp.route("/results")
 def patient_lookup():
     try:
-
+        con = sqlite3.connect(patient_database)
+        cur = con.cursor()
+        cur.execute("SELECT * FROM ClinVar")
+        db_results = cur.fetchall()
+    except Exception as e:
+        print("Database error", e)
         db_results = []
-    except:
-        db_results = []
-    
+    con.close()
     if os.path.exists(Processed_folder):
         files = [file for file in os.listdir(Processed_folder)
                  if os.path.isfile(os.path.join(Processed_folder,file))]
@@ -208,21 +232,45 @@ def patient_lookup():
         )
     else:
         files = []
-
-    return render_template("patient_lookup.html", db_results=db_results, files=files)
+    if os.path.exists(Error_folder):
+        misaligned = [f for f in os.listdir(Error_folder)
+                    if os.path.isfile(os.path.join(Error_folder,f))]
+        misaligned = sorted(
+            misaligned,
+            key= lambda f: os.path.getmtime(os.path.join(Error_folder, f)),
+            reverse=True
+        )
+    else:
+        misaligned = []
+    
+    return render_template("patient_lookup.html", db_results=db_results, files=files, misaligned=misaligned)
 
 @Webapp.route("/view_file",  methods=["GET"])
 def read_file():
-    filename = request.args.get("filename")
-    if not filename:
+    fileprocess = request.args.get("fileprocess")
+    if not fileprocess:
         return  "No file selected!", 400
     
-    viewed_file = os.path.join(Processed_folder, filename)
-    if not os.path.exists(viewed_file):
+    processed_file = os.path.join(Processed_folder, fileprocess)
+    if not os.path.exists(processed_file):
         return "File not found, please refresh and try again."
-    with open(viewed_file, "r", encoding="utf-8") as f:
+    with open(processed_file, "r", encoding="utf-8") as f:
         content = f.read()
     return content
+
+@Webapp.route("/view_misalign",  methods=["GET"])
+def read_misalign():
+    filemisalign = request.args.get("filemisalign")
+    if not filemisalign:
+        return  "No file selected!", 400
+    
+    misaligned_file = os.path.join(Error_folder, filemisalign)
+    if not os.path.exists(misaligned_file):
+        return "File not found, please refresh and try again."
+    with open(misaligned_file, "r", encoding="utf-8") as f:
+        content = f.read()
+    return content
+
 # Run the application
 if __name__ == "__main__":
     Webapp.run(debug=True)

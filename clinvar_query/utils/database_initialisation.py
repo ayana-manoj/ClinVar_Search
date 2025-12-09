c2521ba18f01d9d06ab68e4f182ce4a3c727145c
Harbi Nuur <harbi.nuur@postgrad.manchester.ac.uk>
2025-12-09 14:53:53 +0000

    https://github.com/ayana-manoj/ClinVar_Search/issues/26

---
+++ clinvar_query/utils/database_initialisation.py
@@ -0,0 +1,19 @@
+from clinvar_query.modules.setup_results import create_database
+from clinvar_query.modules.insert_annotated_results import main
+from clinvar_query.logger import logger
+import os
+
+
+def database_initialise(db_file):
+    db_exists = os.path.isfile(db_file)
+    try:
+        if db_exists:
+            return db_file
+
+        else:
+            create_database(db_file)
+            main(db_file)
+            return db_file
+    except Exception as e:
+        logger.critical("Database searching or creating has failed! "
+                        "The app will not have most functionality: {}" .format(e))
